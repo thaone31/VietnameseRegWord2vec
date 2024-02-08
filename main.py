c196e1cdb59afc8b25ee92578c86ea71d1819463
thaone31 <99576488+thaone31@users.noreply.github.com>
2024-02-08 18:19:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 
 # Assuming train_labels, val_labels, test_labels are NumPy arrays
 y_train = to_categorical(train_ratings)
-y_test = to_categorical(test_labels)
+# y_test = to_categorical(test_labels)
 
 y_train_1d = np.argmax(y_train, axis=1)
 y_train_1d = y_train_1d[:, 0]
