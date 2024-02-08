800d33ae7209f4d1f3aecb99f6b36de90b95e818
thaone31 <99576488+thaone31@users.noreply.github.com>
2024-02-08 18:45:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import re,os
-import torch # 're' Replication of text.
+import re,os                                   # 're' Replication of text.
 import numpy as np
 import pandas as pd                         # 'pandas' to manipulate the dataset.
 import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns                       # 'seaborn' to visualize the dataset.
 import tensorflow as tf
-from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.models import Sequential                # 'Sequential' model will be used for training.
 from sklearn.model_selection import train_test_split          # 'train_test_split' for splitting the data into train and test data.
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -12,7 +12,6 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences       # 'pad_s
 from tensorflow.keras.layers import Embedding, Bidirectional,LSTM, Flatten, Dense,Input,Average,Reshape,Dropout,Concatenate,Maximum     # import some layers for training.
 from tensorflow.keras.layers import Conv2D, MaxPool2D,Convolution1D,MaxPooling1D     # import some layers for training.
 from tensorflow.keras.utils import to_categorical
-from sklearn.linear_model import LogisticRegression
 import gensim.models.keyedvectors as word2vec #need to use due to depreceated model
 # import gensim.models.word2vec as Word2Vec #need to use due to depreceated model
 import gc
@@ -20,18 +19,20 @@ from pyvi import ViTokenizer, ViPosTagger # thư viện NLP tiếng Việt
 from tensorflow.keras.utils import plot_model
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
-
 device_name = tf.test.gpu_device_name()
 if device_name != '/device:GPU:0':
   raise SystemError('GPU device not found')
 print('Found GPU at: {}'.format(device_name))
+
+from google.colab import drive
+drive.mount('/content/drive')
 
 import pandas as pd
 from keras.utils import to_categorical
 
 label_mapping = {'pos': 1, 'neu': 2, 'neg': 3}
 
-X_train = pd.read_excel(r"./VietnameseRegWord2vec/Hotel_sentiment-20240207T135933Z-002/Hotel_sentiment/data/Data_train.xlsx")
+X_train = pd.read_excel(r"/content/drive/MyDrive/Hotel_sentiment/data/Data_train.xlsx")
 
 if X_train.isnull().values.any():
     X_train = X_train.dropna()
@@ -47,13 +48,14 @@ y_train = X_train['user_rate'].map(label_mapping)
 
 # Convert to one-hot encoding
 train_labels = to_categorical(y_train - 1, num_classes=3)
+
 import pandas as pd
 from keras.utils import to_categorical
 
 # Assuming 'pos', 'neu', 'neg' are your unique classes
 label_mapping = {'pos': 1, 'neu': 2, 'neg': 3}
 
-X_test = pd.read_excel(r"./VietnameseRegWord2vec/Hotel_sentiment-20240207T135933Z-002/Hotel_sentiment/data/Data_test.xlsx")
+X_test = pd.read_excel(r"/content/drive/MyDrive/Hotel_sentiment/data/Data_test.xlsx")
 
 if X_test.isnull().values.any():
     X_test = X_test.dropna()
@@ -71,12 +73,9 @@ y_test = X_test['user_rate'].map(label_mapping)
 # Convert to one-hot encoding
 test_labels = to_categorical(y_test - 1, num_classes=3)
 
-
-print(X_train['user_rate'].unique())
-
 from gensim.models import KeyedVectors
 
-w2vModel = word2vec.KeyedVectors.load_word2vec_format('./VietnameseRegWord2vec/Hotel_sentiment-20240207T135933Z-002/Hotel_sentiment/baomoi.model.bin', binary=True, limit=50000)
+w2vModel = word2vec.KeyedVectors.load_word2vec_format('/content/drive/MyDrive/Hotel_sentiment/GoogleNews-vectors-negative300.bin', binary=True, limit=50000)
 
 tokenizer = Tokenizer()
 
@@ -89,6 +88,17 @@ train_title = tokenizer.texts_to_sequences(X_train_title)
 train_text = tokenizer.texts_to_sequences(X_train_text)
 test_title = tokenizer.texts_to_sequences(X_test_title)
 test_text = tokenizer.texts_to_sequences(X_test_text)
+
+vocab_size = len(tokenizer.word_index) + 1
+
+max_len_title = 30
+max_len_text = 150
+
+train_title = pad_sequences(train_title, padding = 'post', maxlen = max_len_title)
+train_text = pad_sequences(train_text, padding = 'post', maxlen = max_len_text)
+test_title = pad_sequences(test_title , padding = 'post', maxlen = max_len_title)
+test_text = pad_sequences(test_text , padding = 'post', maxlen = max_len_text)
+
 
 from sklearn.model_selection import train_test_split
 
